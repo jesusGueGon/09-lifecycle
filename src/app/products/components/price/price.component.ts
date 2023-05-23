b8f28f91b24e4978ef701edb1c206a6982ee815d
jesusGueGon <jesus.guerrero@bosonit.com>
2023-05-23 17:56:20 +0200

    Seccion 20 terminada LifeCycle

---
+++ src/app/products/components/price/price.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'products-price',
+  templateUrl: './price.component.html',
+  styleUrls: ['./price.component.css']
+})
+export class PriceComponent implements OnInit, OnChanges, OnDestroy{
+
+  @Input()
+  public price: number = 0;
+
+  public interval$?: Subscription;
+
+  ngOnInit(): void {
+    console.log('Componente HIJO ngOnInit');
+
+    this.interval$ = interval(1000).subscribe( value => console.log(`Tick: ${value}`));
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Componente HIJO ngOnChanges');
+    console.log({changes});
+  }
+
+  ngOnDestroy(): void {
+    console.log('Componente HIJO ngOnDestroy');
+    this.interval$?.unsubscribe();
+  }
+
+
+}
